5b1e946467b799fa8effd961a0b7badfc0c2c80b
Jacob Fleisher <jfleish1@gmail.com>
2015-01-13 08:46:16 -0500

    Added comments to client side controllers

--- public/modules/core/controllers/game-controller.client.controller.js
+++ public/modules/core/controllers/game-controller.client.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
-angular.module('core').controller('GameController', ['$scope', '$location', 'games', 'game', function($scope, $location, games, game) {
+angular.module('core')
+	//  Controller for the single game info page
+	.controller('GameController', ['$scope', '$location', 'games', 'game', function($scope, $location, games, game) {
+		
+		// Variables for holding the data of the game
 		$scope.name = game.name;
 		$scope.developer = game.developer;
 		$scope.year = game.year;
@@ -8,7 +12,9 @@ angular.module('core').controller('GameController', ['$scope', '$location', 'gam
 		$scope.issues = game.issues;
 		// $scope.title = "hi";
 		
+		// Function for adding an issue for the game
 		$scope.addIssue = function() {
+			// Issue information
 			var d = new Date();
 			var date = d.toDateString();
 			var issue = {
@@ -17,18 +23,23 @@ angular.module('core').controller('GameController', ['$scope', '$location', 'gam
 				type: $scope.type,
 				dateCreated: date
 			};
-			games.addIssue(game._id, issue).success(function (userIssue) {
-				// Go to the page of the newly created issue				
-				console.log(userIssue._id);
-				// $window.location.href = '#!/games/' + game._id + '/issues/' + userIssue._id;
-				$location.path('/games/' + game._id + '/issues/' + userIssue._id);
-			});
+			//  Adds the issue to the game
+			games.addIssue(game._id, issue)
+				// If successful go to the page of the newly created issue				
+				.success(function (userIssue) {
+					// console.log(userIssue._id);
+					// $window.location.href = '#!/games/' + game._id + '/issues/' + userIssue._id;
+					$location.path('/games/' + game._id + '/issues/' + userIssue._id);
+				});
 		};
 
+		//  Allows an issue to be upvoted
 		$scope.upvoteIssue = function (issue) {
 			games.upvoteIssue(game._id, issue._id);
 			// console.log(issue);
 		};
+
+		// Creates a toggle button to show/hide the issue fields for adding an issue
 		$(".addIssueButton").click(function() {
 			$(".addIssueDropdown").slideToggle();
 		});
--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('core')
+	//  Controls the header on all pages.  User login services, searching games, and adding games to the DB
 	.controller('HeaderController', ['$scope', '$location', 'Authentication', 'Menus', 'games', function($scope, $location, Authentication, Menus, games) {
+		//  Sets the current user logged in
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
+		
+		//  Sets the search text to empty
 		$scope.searchText = "";
 
 		$scope.toggleCollapsibleMenu = function() {
@@ -21,14 +25,17 @@ angular.module('core')
 			$event.stopPropagation();
 		};
 
-		// Search funtion
+		// Search funtion using the searchText entered by a user to find a game
 		$scope.search = function() {
 			$location.path('/search/' + $scope.searchText);
 		}
 
+		// Variables for adding a game to the DB: name, developer, year.
 		$scope.name = "";
 		$scope.developer = "";
 		$scope.year;
+
+		// Function for adding a game to the DB.  All fields must be entered.
 		$scope.addGame = function () {
 			if ($scope.name === "" || $scope.developer === "" || $scope.year === undefined) { return; }
 			games.create({
